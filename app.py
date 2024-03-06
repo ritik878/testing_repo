dc49d194893970eec8857739acf70a9c577255d1
ritik878 <ritik.goel@magicedtech.com>
2024-03-06 12:19:01 +0530

    solved

--- app.py
+++ app.py
@@ -2,18 +2,15 @@
 from flask import Flask
 from flask_cors import CORS  # Import the CORS extension
 import argparse
-
+import os
 from flask_sqlalchemy import SQLAlchemy
-
 app = Flask(__name__)
 CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:sa123@127.0.0.1/codecrafter"
 db = SQLAlchemy(app)
-
 @app.route('/')
 def hello_world():
     return 'Hello, World!hgh'
-
 @app.route("/name/<name>",methods=['GET'])
 def saymyname(name):
     return f"Hii {name}"
@@ -21,6 +18,5 @@ parser = argparse.ArgumentParser(description="Run Flask application")
 parser.add_argument('-p', '--port', type=int, default=5000, help='Port to run the app on')
 args = parser.parse_args()
 port = args.port
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port, debug=True)
