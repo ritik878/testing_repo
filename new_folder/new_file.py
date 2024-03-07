14e4a44981dcfaafe01ace4bd85973367da0d876
ritik878 <ritik.goel@magicedtech.com>
2024-03-07 16:29:04 +0530

    resolved

--- new_folder/new_file.py
+++ new_folder/new_file.py
@@ -5,9 +5,5 @@ print("G")
 print("That is not good")
 print("Merge conflict")
 print("Changes committed")
-<<<<<<< Updated upstream
-print('New changes phirse lo aasdbdsbn chle gye hai')
-=======
 print('New changes phirs savabsvsae lo aa chle gye hai')
->>>>>>> Stashed changes
 
