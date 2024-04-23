914266d002a75117a4c55dd71ad4707a8e2ca541
ritik878 <137982608+ritik878@users.noreply.github.com>
2024-04-23 16:35:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,37 +2,18 @@
 from flask import Flask
 from flask_cors import CORS  # Import the CORS extension
 import argparse
-import argsss
-import ossss
-from flask_sqlalchemy import SQLAlchemy
+import platform
+import args
+from flask_sqlalchemy
+from flask_cors import CORS
+from flask import request
 app = Flask(__name__)
-CORS(app)
+CORS(app) 
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:sa123@127.0.0.1/codecrafter"
 db = SQLAlchemy(app)
 @app.route('/')
 def hello_world():
-    return 'Hello, World!hgh'
-my changes
-@app.route("/name/<name>",methods=['GET'])
-def saymyname(name):
-    return f"Hii {name}"
-@app.route("/create_jira", methods=['POST'])
-def create_jira():
-    data = request.get_json()
-    # code to create a new jira issue with the data
-    from jira import JIRA
-    # Create a connection object
-    jira = JIRA(basic_auth=('username', 'password'), options={'server': 'https://your_jira_server_url'})
-    # Create an issue
-    issue_dict = {
-        'project': {'key': data['project_key']},
-        'summary': data['summary'],
-        'description': data['description'],
-        'issuetype': {'name': data['issue_type']},
-    }
-    issue = jira.create_issue(fields=issue_dict)
-    return 'Jira issue created successfully'
-
+    return 'Hello, World!'
 parser = argparse.ArgumentParser(description="Run Flask application")
 parser.add_argument('-p', '--port', type=int, default=5000, help='Port to run the app on')
 args = parser.parse_args()
